f0254d17d0dc940e65c2fb08e4633964033a2eec
mkouddar <77578490+mkouddar@users.noreply.github.com>
2021-01-17 19:25:59 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -18,6 +18,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
+sudo apt-get install foobillardplus
 sudo adduser KOUDDAR chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
